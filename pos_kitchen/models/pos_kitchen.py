eefb37018f6fa7fc93f6e591895ca30c28997681
SalaiThomas <salaithomas582@gmail.com>
2025-02-23 23:20:21 +0630

    create some field and add form and tree view

--- pos_kitchen/models/pos_kitchen.py
+++ pos_kitchen/models/pos_kitchen.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class PosKitchen(models.Model):
     _name = 'pos.kitchen'
 
-    sequence= fields.Char("Sequence")
-
+    sequence = fields.Char("Sequence")
+    pos_config_id = fields.Many2one("pos.config",string="Allowed POS",help="Allowed POS For Kitchen")
+    pos_category = fields.Many2one("pos.category",string="Allowed POS Category",help="Allowed POS Category For Kitchen")
 
